40d4b8f6297ca99a8fda7beb9b4f29bf7b5d2945
Nikita Yutanov <nikitayutanov@gmail.com>
2023-03-16 01:07:38 +0300

    Configure styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import 'assets/styles/index.scss';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
