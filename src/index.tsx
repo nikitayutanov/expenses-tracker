423393f28c0b269d240650734a716dee4d2323e1
Nikita Yutanov <nikitayutanov@gmail.com>
2023-03-19 02:46:07 +0300

    Fix 404 error on deployed build

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
 import { ModalsProvider } from '@mantine/modals';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { App } from './App';
 import 'assets/styles/index.scss';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: (
       <ModalsProvider>
         <App />
       </ModalsProvider>
-    ),
-  },
+    )
+  }
 ]);
 
 root.render(
